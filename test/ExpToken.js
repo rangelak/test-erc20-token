9c071d725cbd52bca4ea72becec4d8f34b4ca215
rangelak <r.milouchev@gmail.com>
2019-05-31 10:48:16 +0300

    initial commit

--- test/ExpToken.js
+++ test/ExpToken.js
@@ -11,7 +11,7 @@ contract('ExpToken', function(accounts){
 
 		// checks whether totalSupply is correct
 		}).then(function(totalSupply){
-			assert.equal(totalSupply.toNumber(), 100000000000, 'total supply equals 100,000,000,000');
+			assert.equal(totalSupply.toNumber(), 100000000000, 'total supply set to 100,000,000,000');
 		});
 	});
 });
